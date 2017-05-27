2c88a3ea361545b9e4e382ebf25ab3bb6c48ea0a
KajanM <kajan.14@cse.mrt.ac.lk>
2017-05-27 17:50:51 +0530

    networking and manipulating dom with jquery

--- js/index.js
+++ js/index.js
@@ -29,7 +29,6 @@ showItThenf("hello again", function () {
 
     var resultsList = $("#resultsList");
     resultsList.text("This is from jQuery");
-
     var toggleButton = $("#toggleButton");
     toggleButton.on("click", function () {
         resultsList.toggle(500);
@@ -40,7 +39,7 @@ showItThenf("hello again", function () {
 
     });
 
-    var results = [{
+   /* var results = [{
           name: "jQuery",
            language: "JavaScript",
            score: 4.5,
@@ -61,25 +60,47 @@ showItThenf("hello again", function () {
              login: "shawnwildermuth",
              id: 123456
            }
-         }];
+         }];*/
 
-    resultsList.empty();
-    $.each(results, function (i, item) {
-        var newResult = $("<div class = 'result'>" +
-            "<div class = 'title'>" + item.name + "</div>" +
+    var githubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars";
+
+   /* $.get(githubSearch)
+        .success(function (r) {
+        console.log(r.items.length);
+        displayResults(r.items);
+    })
+        .fail(function (err) {
+        console.log("Failed to query GitHub");
+    });*/
+
+    $.get(githubSearch, function(r) {
+            //console.log(r.items.length);
+            displayResults(r.items);
+        }/*, function() {
+            console.log("Failed to query GitHub");
+        }*/);
+
+
+    function displayResults(results) {
+        resultsList.empty();
+        $.each(results, function (i, item) {
+            var newResult = $("<div class = 'result'>" +
+                "<div class = 'title'>" + item.name + "</div>" +
                 "<div>Language: " + item.language + "</div>" +
                 "<div>Owner: " + item.owner.login + "</div>" +
                 "</div>");
 
-        newResult.hover(function () {
-            //make it darker
-            $(this).css("background-color", "lightgray");
-        }, function () {
-            //reverse
-            $(this).css("background-color", "transparent");
-            
-        });
-        resultsList.append(newResult);
-    })
+            newResult.hover(function () {
+                //make it darker
+                $(this).css("background-color", "lightgray");
+            }, function () {
+                //reverse
+                $(this).css("background-color", "transparent");
+
+            });
+            resultsList.append(newResult);
+        })
+    }
+
 
 });
