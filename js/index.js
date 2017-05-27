af7ee070c954b0ed57009eca255623c6da4f5399
KajanM <kajan.14@cse.mrt.ac.lk>
2017-05-27 11:41:44 +0530

    using callback

--- js/index.js
+++ js/index.js
@@ -4,8 +4,23 @@
 
 "use strict;"
 
-var msg = "hello javascript";
-console.log(msg);
+/*var msg = "hello javascript";
+console.log(msg);*/
 
 var resultsDiv = document.getElementById("results");
 resultsDiv.innerHTML = "<p>This is from JavaScript</p>";
+
+var showIt = function (msg) {
+    console.log(msg);
+
+};
+
+function showItThenf(msg, callback){
+    console.log(msg);
+    callback();
+}
+
+showIt("hello");
+showItThenf("hello again", function () {
+    console.log("callback called")
+});
